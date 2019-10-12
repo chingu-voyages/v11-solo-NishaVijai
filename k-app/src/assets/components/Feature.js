536e9dc2d31fa956526c1f76e701672c58095234
Nisha <nishavijai@gmail.com>
2019-10-12 17:07:12 +0200

    UPDATE FEATURE COMPONENT
    Add updated feature component, right side design have to be updated

--- k-app/src/assets/components/Feature.js
+++ k-app/src/assets/components/Feature.js
@@ -10,50 +10,45 @@ export class Feature extends React.Component {
 					<hr className="thin-horiz-line" />
 				</div>
 
-				<div className="design-text-container">
+				<div className="design-text-container feature-container">
 					<div className="design-text feature-left">
 						<div className="container-title feature-title">
 							<h4>featured project</h4>
 						</div>
 						<a
-							className="design-link tilt-five-text f-p-l"
+							className="design-link tilt-five"
 							href="https://www.kickstarter.com/projects/tiltfive/holographic-tabletop-gaming?ref=section-homepage-featured-project"
 							target="_blank"
 							rel="noopener  noreferrer"
 						>
-							<img className="left-image f-p-l" src={require('../images/tiltfive.jpg')} alt="" /> <br />
-							<p className="tilt-text tilt-heading">Tilt Five: Holographic Tabletop Gaming</p>
+							<img className="" src={require('../images/tiltfive.jpg')} alt="" /> <br />
+							<p>Tilt Five: Holographic Tabletop Gaming</p>
 						</a>
 
 						<a
-							className="link-no-underline f-p-l"
+							className="link-no-underline feature-no-underline"
 							href="https://www.kickstarter.com/projects/tiltfive/holographic-tabletop-gaming?ref=section-homepage-featured-project"
 							target="_blank"
 							rel="noopener  noreferrer"
 						>
 							<br />
-							<p className="tilt-text tilt-p">Take game night to another dimension.</p>
+							<p>Take game night to another dimension.</p>
 						</a>
 						<a
-							className="black-link f-p-l"
+							className="black-link"
 							href="https://www.kickstarter.com/projects/tiltfive/holographic-tabletop-gaming?ref=section-homepage-featured-project"
 							target="_blank"
 							rel="noopener  noreferrer"
 						>
 							<br />
-							<p className="tilt-text">
+							<p>
 								By <span className="name-underline">Tilt Five</span>
 							</p>
 						</a>
 					</div>
 
 					<hr className="thin-vertical-line" />
-					{/* <hr /> */}
 
-					{/* <div className=""> */}
-					{/* <div className="container-title feature-title">
-							<h4>recommended</h4>
-						</div> */}
 					<div className="feature-right">
 						<div className="container-title feature-title">
 							<h4>recommended</h4>
@@ -61,17 +56,9 @@ export class Feature extends React.Component {
 						<div className="feature-right-content">
 							<div className="f-right first">
 								<div className="image-right">
-									{/* <a
-											className=""
-											href="https://www.kickstarter.com/projects/nichtlustig/moviecliches?ref=section-homepage-projectcollection-3-staff-picks-newest"
-											target="_blank"
-											rel="noopener  noreferrer"
-										>
-											<img className="" src={require('../images/ff-movie.png')} alt="" /> <br />
-											<p>MOVIE CLICHÉS - the card game</p>
-										</a> */}
 									<img className="" src={require('../images/ff-movie.png')} alt="" />
 								</div>
+
 								<div className="p-right">
 									<p className="p-text">MOVIE CLICHÉS - the card game</p>
 									<p className="right-green-font">220% funded</p>
@@ -87,6 +74,7 @@ export class Feature extends React.Component {
 										</p>
 									</a>
 								</div>
+
 								<div className="heart-right">
 									<img
 										className=""
@@ -94,23 +82,8 @@ export class Feature extends React.Component {
 										alt=""
 										title="Remind Me"
 									/>
-									{/* <p className="heart-desc">Remind Me</p> */}
 								</div>
 							</div>
-							{/* <div>
-									<p>220% funded</p>
-								</div>
-								<div>
-									<a
-										className="black-link"
-										href="https://www.kickstarter.com/projects/nichtlustig/moviecliches?ref=section-homepage-projectcollection-3-staff-picks-newest"
-										target="_blank"
-										rel="noopener  noreferrer"
-									>
-										<br />
-										By <span className="name-underline">Joscha Sauer</span>
-									</a>
-								</div> */}
 
 							<hr className="thin-horiz-line" />
 
@@ -118,6 +91,7 @@ export class Feature extends React.Component {
 								<div className="image-right">
 									<img className="" src={require('../images/to-everspace.jpg')} alt="" />
 								</div>
+
 								<div className="p-right">
 									<p>EVERSPACE 2</p>
 
@@ -134,6 +108,7 @@ export class Feature extends React.Component {
 										</p>
 									</a>
 								</div>
+
 								<div className="heart-right">
 									<img
 										className=""
@@ -141,7 +116,6 @@ export class Feature extends React.Component {
 										alt=""
 										title="Remind Me"
 									/>
-									{/* <p className="heart-desc">Remind Me</p> */}
 								</div>
 							</div>
 
@@ -151,6 +125,7 @@ export class Feature extends React.Component {
 								<div className="image-right">
 									<img className="" src={require('../images/to-legends.jpg')} alt="" />
 								</div>
+
 								<div className="p-right">
 									<p>Time of Legends: Joan of Arc 1.5</p>
 
@@ -166,6 +141,7 @@ export class Feature extends React.Component {
 										</p>
 									</a>
 								</div>
+
 								<div className="heart-right">
 									<img
 										className=""
@@ -173,61 +149,11 @@ export class Feature extends React.Component {
 										alt=""
 										title="Remind Me"
 									/>
-									{/* <p className="heart-desc">Remind Me</p> */}
 								</div>
 							</div>
 
 							<hr className="thin-horiz-line" />
 						</div>
-
-						{/* <div className="f-right second">
-								<a
-									className=""
-									href="https://www.kickstarter.com/projects/rockfishgames/everspace-2?ref=section-homepage-projectcollection-3-staff-picks-popular"
-									target="_blank"
-									rel="noopener  noreferrer"
-								>
-									<img className="" src={require('../images/to-everspace.jpg')} alt="" /> <br />
-									<p>EVERSPACE 2</p>
-								</a>
-								<p>EVERSPACE 2</p>
-								<br />
-								<p>35% funded</p>
-								<br />
-								<a
-									className="black-link"
-									href="https://www.kickstarter.com/projects/rockfishgames/everspace-2?ref=section-homepage-projectcollection-3-staff-picks-popular"
-									target="_blank"
-									rel="noopener  noreferrer"
-								>
-									<br />
-									By <span className="name-underline">ROCKFISH Games</span>
-								</a>
-							</div> */}
-
-						{/* <div className="f-right third">
-								<a
-									className="homestretch-link"
-									href="https://www.kickstarter.com/projects/1162110258/time-of-legends-joan-of-arc-15?ref=section-homepage-projectcollection-4-staff-picks-popular"
-									target="_blank"
-									rel="noopener  noreferrer"
-								>
-									<img className="" src={require('../images/to-legends.jpg')} alt="" /> <br />
-									<p>Time of Legends: Joan of Arc 1.5</p>
-								</a>
-
-								<p>617% funded</p>
-
-								<a
-									className="black-link"
-									href="https://www.kickstarter.com/projects/1162110258/time-of-legends-joan-of-arc-15?ref=section-homepage-projectcollection-4-staff-picks-popular"
-									rel="noopener  noreferrer"
-								>
-									<br />
-									By <span className="name-underline">Mythic Games, Inc.</span>
-								</a>
-							</div> */}
-						{/* </div> */}
 					</div>
 				</div>
 			</div>
